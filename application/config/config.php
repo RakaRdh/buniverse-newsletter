a479e0fa29a8b69b9c2d8d6d98337b233b68f50d
RakaRdh <rakaherdika15@gmail.com>
2026-08-28 11:28:31 +0700

    fix: add is_string checks to prevent PHP 8.1+ TypeError in strpos

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,8 @@ $is_vercel = (strpos(__FILE__, '/var/task') === 0)
              || (getenv('VERCEL') == '1')
              || isset($_SERVER['VERCEL']) 
              || isset($_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL']) 
-             || (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && strpos($_SERVER['HTTP_X_FORWARDED_HOST'], 'vercel.app') !== false) 
-             || (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'vercel.app') !== false);
+             || (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && is_string($_SERVER['HTTP_X_FORWARDED_HOST']) && strpos($_SERVER['HTTP_X_FORWARDED_HOST'], 'vercel.app') !== false) 
+             || (isset($_SERVER['HTTP_HOST']) && is_string($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'vercel.app') !== false);
 
 $is_secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
              || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
@@ -40,7 +40,7 @@ if ($is_vercel) {
         $http_host = $_SERVER['HTTP_X_FORWARDED_HOST'];
     } elseif (isset($_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL'])) {
         $http_host = $_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL'];
-    } elseif (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'localhost') === false) {
+    } elseif (isset($_SERVER['HTTP_HOST']) && is_string($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'localhost') === false) {
         $http_host = $_SERVER['HTTP_HOST'];
     } else {
         $http_host = 'buniverse-newsletter.vercel.app';
