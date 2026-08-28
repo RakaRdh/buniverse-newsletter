630b23d5435c7b96f99071d99910ee8f52d3f38d
RakaRdh <rakaherdika15@gmail.com>
2026-08-28 12:56:26 +0700

    test: temporarily hardcode base_url to test CI config loading

--- application/config/config.php
+++ application/config/config.php
@@ -22,36 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
-$is_vercel = (strpos(__FILE__, '/var/task') === 0)
-             || (getenv('VERCEL') == '1')
-             || isset($_SERVER['VERCEL']) 
-             || isset($_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL']) 
-             || (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && is_string($_SERVER['HTTP_X_FORWARDED_HOST']) && strpos($_SERVER['HTTP_X_FORWARDED_HOST'], 'vercel.app') !== false) 
-             || (isset($_SERVER['HTTP_HOST']) && is_string($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'vercel.app') !== false);
-
-$is_secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-             || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
-             || $is_vercel;
-
-$protocol = $is_secure ? 'https' : 'http';
-
-if ($is_vercel) {
-    if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-        $http_host = $_SERVER['HTTP_X_FORWARDED_HOST'];
-    } elseif (isset($_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL'])) {
-        $http_host = $_SERVER['HTTP_X_VERCEL_DEPLOYMENT_URL'];
-    } elseif (isset($_SERVER['HTTP_HOST']) && is_string($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'localhost') === false) {
-        $http_host = $_SERVER['HTTP_HOST'];
-    } else {
-        $http_host = 'buniverse-newsletter.vercel.app';
-    }
-    $path = '/';
-} else {
-    $http_host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost:8000';
-    $path = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-}
-
-$config['base_url'] = $protocol . "://" . $http_host . $path;
+$config['base_url'] = 'https://buniverse-newsletter.vercel.app/';
 
 if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], 'debug_base_url') !== false) {
     echo "RESOLVED BASE URL: " . $config['base_url'] . "\n";
