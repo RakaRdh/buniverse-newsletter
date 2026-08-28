1e141cc66b0e1c9b09c96ccacaa77457dcfc4668
RakaRdh <rakaherdika15@gmail.com>
2026-08-28 12:52:59 +0700

    debug: add api/test_ci_config.php and update vercel.json

---
+++ api/test_ci_config.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/plain');
+chdir(dirname(__DIR__));
+define('BASEPATH', __DIR__ . '/../system/');
+define('APPPATH', __DIR__ . '/../application/');
+define('VIEWPATH', __DIR__ . '/../application/views/');
+define('FCPATH', __DIR__ . '/../');
+
+require_once BASEPATH . 'core/Common.php';
+
+$config =& get_config();
+echo "COMMON CONFIG BASE_URL: " . ($config['base_url'] ?? 'NOT SET') . "\n";
+
+require_once BASEPATH . 'core/Config.php';
+$ci_config = new CI_Config();
+echo "CI_CONFIG BASE_URL: " . $ci_config->item('base_url') . "\n";
