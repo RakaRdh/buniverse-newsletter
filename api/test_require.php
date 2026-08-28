e375058ad53522d1fc7c49774e1b87515ec965e4
RakaRdh <rakaherdika15@gmail.com>
2026-08-28 12:59:23 +0700

    debug: add api/test_require.php and update vercel.json

---
+++ api/test_require.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: text/plain');
+
+function test_include() {
+    static $config;
+    define('BASEPATH', '1');
+    require __DIR__ . '/../application/config/config.php';
+    echo "=== INSIDE FUNCTION ===\n";
+    print_r($config);
+}
+
+test_include();
